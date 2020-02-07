d86dafdedf1a80f7c612a956752536e068e3f1e9
denis <025896321Denis>
2020-02-07 18:39:49 +0200

    add         binding = DataBindingUtil.setContentView(this, R.layout.activity_game);

--- app/src/main/java/com/parik/bingo/GameActivity.java
+++ app/src/main/java/com/parik/bingo/GameActivity.java
@@ -7,15 +7,23 @@ import android.os.Bundle;
 
 import com.parik.bingo.databinding.ActivityGameBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameActivity extends AppCompatActivity {
 
     private ActivityGameBinding binding;
+    private List<CharSequence> celektedNambers = new ArrayList<CharSequence>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_game);
 
+        binding.baton1.setOnClickListener(v -> {
+          celektedNambers.add(binding.baton1.getText());
+           //TODO add baton selector and chehg coor en cre selektor
+        });
 
 
     }
