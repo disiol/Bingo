a7dc3f39e5ec494603ce95613b7fdfc4f9f3ac0e
denis <025896321Denis>
2020-02-07 16:37:07 +0200

    Initial commit

---
+++ app/src/main/java/com/parik/bingo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.parik.bingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void showGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+    }
+
+}
