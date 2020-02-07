8ed8d73d34f3e5a7275800b427568703a5617b6a
denis <025896321Denis>
2020-02-07 18:33:10 +0200

    add         binding = DataBindingUtil.setContentView(this, R.layout.activity_game);

--- app/src/main/java/com/parik/bingo/GameActivity.java
+++ app/src/main/java/com/parik/bingo/GameActivity.java
@@ -1,14 +1,22 @@
 package com.parik.bingo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.parik.bingo.databinding.ActivityGameBinding;
+
 public class GameActivity extends AppCompatActivity {
+
+    private ActivityGameBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_game);
+
+
+
     }
 }
